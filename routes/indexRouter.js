cc2dcaa0be27d616f6c1f8eeaa876068829cb23a
Amroshwayat <amrshwayat85@gmail.com>
2026-06-01 01:44:43 +0300

    Fix routes and 404 issu3

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router(); // هذا هو السطر الذي كان مفقوداً أو خاطئاً
+const bookController = require('../controllers/bookController'); // تأكد من المسار
 
+// هذا هو المسار الذي تحتاجه ليختفي خطأ Cannot GET /
+router.get('/', bookController.getPublicBooks);
 // الآن يمكنك استخدام router
 router.get('/about1', (req, res) => {
     res.render('about');
